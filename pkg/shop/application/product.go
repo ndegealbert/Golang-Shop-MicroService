a4398fa534526092e0413b3117b8c34761b08dc8
ndegealbert <albertndege2@gmail.com>
2022-04-18 23:00:48 +0300

    add price interface

--- pkg/shop/application/product.go
+++ pkg/shop/application/product.go
@@ -5,7 +5,7 @@ type productReadModel interface {
 }
 
 type ProductService struct {
-	repo      products.Repository //sorage
+	repo      products.Repository //storage
 	readModel productReadModel    //of type
 }
 
@@ -17,6 +17,20 @@ func (s ProductService) AllProducts() {
 
 }
 
-func (s productService) AddProduct() {
+type AddProductCommand struct {
+	ID            string
+	Name          string
+	Description   string
+	PriceCent     units
+	priceCurrency string
+}
+
+func (s ProductService) AddProduct(cmd AddProductCommand) error {
+	//NewPrice(cmd.PriceCent, cmd.priceCurrency)
+	//newProduct(products.ID(cmd.ID), cmd.Description, cmd.Name, cmd.price)
+	price.NewPrice(cmd.PriceCent, cmd.priceCurrency)
+	products.newProduct(products.ID(cmd.ID), cmd.Description, cmd.Name, price)
+
+	s.repo.Save
 
 }
--- pkg/shop/infrastracture/products/memory.go
+++ pkg/shop/infrastracture/products/memory.go
@@ -1,1 +1,17 @@
 package products
+
+// MemoryRepository temp memory for storage
+type MemoryRepository struct {
+}
+
+func Save() {
+
+}
+
+func ById() {
+
+}
+
+func AllProduts() {
+
+}
