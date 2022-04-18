0a3492280f6adb26d0cb937c94d7d836e842c1e8
ndegealbert <albertndege2@gmail.com>
2022-04-18 08:23:31 +0300

    add ignore of the idea

--- cmd/microservice/order/main.go
+++ cmd/microservice/order/main.go
@@ -1,3 +1,30 @@
-package order
-
 //order is separate from other services payment && shop
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	log.Println("starting micro-service")
+	ctx := cmd.Context()
+
+	r , closeFn := createOrderMicroservice()
+
+	defer closeFn()
+	server := &http.server(Addr:os.Getenv("SHOP_ORDER_SERVICE_BIND_ADDR" ),Hanler:r)
+
+	go func() {
+		if err := server.ListenAddServer(),err != http.ErrServerClosed{
+			panic()
+		}
+
+		<-ctx.Done()
+		log.Println("closing order microservice")
+		if err := server.Close(); err !=nill{
+			pamic(err)
+		}
+	}()
+}
